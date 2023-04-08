4fb9f1c380c2d29369b68093548ebbe1cb83f95b
EuJinnLucaShow <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-04-08 07:28:15 +0000

    Deploying to gh-pages from @ EuJinnLucaShow/goit-js-hw-09-v2@34131735545fbff2f205ced9d3aeb5917a77be9f 🚀

---
+++ 01-color-switcher.a592ad95.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a;e.addEventListener("click",(()=>{a=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a592ad95.js.map
