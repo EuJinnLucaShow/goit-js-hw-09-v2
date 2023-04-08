34131735545fbff2f205ced9d3aeb5917a77be9f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-08 10:27:49 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+// document.querySelector('[data-start]').addEventListener('click', () => {
+//   timerId = setInterval(() => {
+//     document.querySelector('body').style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// });
+
+// document.querySelector('[data-stop]').addEventListener('click', () => {
+//   clearInterval(timerId);
+// });
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true; // Додаємо атрибут "disabled" до кнопки "Start"
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false; // Забираємо атрибут "disabled" з кнопки "Start"
+});
