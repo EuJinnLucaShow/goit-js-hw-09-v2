3ad87cdbe5248edc60d6cec4f1efb47f033c9c79
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-09 09:19:05 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,12 @@
-// Описаний в документації
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
 import 'flatpickr/dist/themes/dark.css';
 import Notiflix from 'notiflix';
 
-// const input = document.getElementById('datetime-picker');
-
+const input = document.getElementById('datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 startBtn.disabled = true;
 let date;
+let timerId;
 
 const options = {
   locale: 'default',
@@ -23,41 +21,41 @@ const options = {
         'Please choose a date in the future',
         'Ok'
       );
-      startBtn.disabled = true;
     } else {
       startBtn.disabled = false;
+      input.disabled = true;
       date = selectedDates;
     }
   },
 };
 
-flatpickr('#datetime-picker', options);
+flatpickr(input, options);
 
 function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+  if (ms <= 0) {
+    clearInterval(timerId);
+    return { day: '00', hour: '00', minute: '00', second: '00' };
+  }
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const day = Math.floor(ms / (1000 * 60 * 60 * 24));
+  const hour = Math.floor((ms / (1000 * 60 * 60)) % 24);
+  const minute = Math.floor((ms / (1000 * 60)) % 60);
+  const second = Math.floor((ms / 1000) % 60);
 
-  const dataDays = document.querySelector('[data-days]');
-  const dataHours = document.querySelector('[data-hours]');
-  const dataMinutes = document.querySelector('[data-minutes]');
-  const dataSeconds = document.querySelector('[data-seconds]');
-  dataDays.textContent = `${addLeadingZero(days)}`;
-  dataHours.textContent = `${addLeadingZero(hours)}`;
-  dataMinutes.textContent = `${addLeadingZero(minutes)}`;
-  dataSeconds.textContent = `${addLeadingZero(seconds)}`;
+  const days = document.querySelector('[data-days]');
+  const hours = document.querySelector('[data-hours]');
+  const minutes = document.querySelector('[data-minutes]');
+  const seconds = document.querySelector('[data-seconds]');
+  days.textContent = `${addLeadingZero(day)}`;
+  hours.textContent = `${addLeadingZero(hour)}`;
+  minutes.textContent = `${addLeadingZero(minute)}`;
+  seconds.textContent = `${addLeadingZero(second)}`;
 
   function addLeadingZero(value) {
     return value.toString().padStart(2, '0');
   }
 
-  return { days, hours, minutes, seconds };
+  return { day, hour, minute, second };
 }
 
 startBtn.addEventListener('click', () => {
